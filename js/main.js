37e7d9e95765f369e8c79ae43202b094dd7d14fd
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-06-27 03:59:24 -0500

    Update: Added new files, Photos, Banner, Buttons

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//mouseover
+let btn1 = document.querySelector("#btn1");
+btn1.addEventListener("mouseover", function (event){
+    event.target.style.background = "white";
+    event.target.style.color = "#e84c3d"
+})
